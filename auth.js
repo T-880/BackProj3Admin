73b65c5bb6ab31456c0a87273e1c08dfc1587c51
Trella <mr.trella@hotmail.com>
2026-05-18 12:21:56 +0200

    Skapat auth-modul för hantering av JWT token i localStorage

---
+++ auth.js
@@ -0,0 +1,12 @@
+function saveToken(token) {
+  localStorage.setItem("token", token);
+}
+
+function getToken() {
+  return localStorage.getItem("token");
+}
+
+function logout() {
+  localStorage.removeItem("token");
+  window.location.href = "login.html";
+}
